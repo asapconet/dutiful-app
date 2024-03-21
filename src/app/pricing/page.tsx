79a936eff9c8af54912e34b299ebca74fb749630
ASAP_A1 <aaronrickymeek@gmail.com>
2024-03-21 14:32:04 +0100

    chore: add dependencies and style configurations

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pricing() {
+  return <section className='min-h-[80vh]'>Pricing here</section>;
+}
+
+export default Pricing;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        circular: ['"Circular Std"', "san-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        box_shadow: "0px -4px 20px 0px #92929240",
+        btn_shadow: " 2px 8px 8px 0px #56565640",
+      },
+      colors: {
+        primary: "#532F82",
+        primary_text: "#230B34",
+        secondary_text: "#B6B6E5",
+        disabled_text: "#B1BDCA",
+        light_gray: "#686868",
+        dark_gray: "#383838",
+        border_gray: "#EEEEFF",
+        border_purple: "#603F8B",
+        input_bg: "#E7EAF1",
+        input_border: "#E7EAF1",
+      },
+      screens: {
+        "3xl": "2100px",
       },
     },
   },
